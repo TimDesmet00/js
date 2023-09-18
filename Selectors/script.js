1947e928fe5efb2326172bc4a641d581eeae4c51
tim <timdesmet00@gmail.com>
2023-09-18 15:30:42 +0200

    fin de la partie selector

--- Selectors/script.js
+++ Selectors/script.js
@@ -1,2 +1,45 @@
 /*
-*/
+ */
+
+// première exercise
+const imp = document.querySelectorAll(".important");
+imp.forEach((element) => {
+  element.setAttribute("title", "This is an important item");
+});
+
+// deuxième exercice
+
+const pictures = document.querySelectorAll("img");
+
+pictures.forEach((picture) => {
+  if (picture.classList.contains("important")) {
+  } else {
+    picture.style.display = "none";
+  }
+});
+
+// troisième exercice
+
+const paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach((paragraph) => {
+  const classes = paragraph.classList;
+  const content = paragraph.textContent.trim();
+
+  if (classes.length > 0) {
+    console.log(`class: ${classes}. contenu: ${content}`);
+  } else {
+    console.log(`contenu: ${content}`);
+  }
+});
+
+// quatrième exercice
+
+paragraphs.forEach((paragraph) => {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  let randomColor = `rgb(${red}, ${green}, ${blue})`;
+  paragraph.style.color = randomColor;
+});
